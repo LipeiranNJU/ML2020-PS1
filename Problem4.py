ed4984ad18c4a34f0075a2a8a79f51e0e28445d8
李佩然 <513962077@qq.co>
2020-03-08 20:07:34 +0800

    finished hw1

--- Problem4.py
+++ Problem4.py
@@ -1,2 +1,11 @@
-print("Hello World!")
-## some test in pycharm
+import numpy as np
+from math import pow
+r = np.array([3.2, 3.8, 1.2, 4, 2.8])
+k = 5
+N = 5
+tx2 = (12 * N) / (k * (k + 1)) * (np.dot(r, r) - k * pow(k + 1, 2) / 4)
+tf = ((N - 1) * tx2) / (N * (k - 1) - tx2)
+print(tx2)
+print(tf)
+CD005 = (2.728 * pow(k*(k+1)/6/N, 0.5))
+print(CD005)
